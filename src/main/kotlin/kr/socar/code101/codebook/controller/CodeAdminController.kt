b60ecf00511eb56f17f03dae84cc3ef7bf3384e2
BoKyeong-Kim <bokyeong3659@gmail.com>
2021-07-15 01:57:05 +0900

    modify ComCodeGroupHistoryRepository and Controller

--- src/main/kotlin/kr/socar/code101/codebook/controller/CodeAdminController.kt
+++ src/main/kotlin/kr/socar/code101/codebook/controller/CodeAdminController.kt
@@ -1,9 +1,11 @@
 package kr.socar.code101.codebook.controller
 
+import kr.socar.code101.codebook.model.ComCodeGroupHistory
 import kr.socar.code101.codebook.repository.ComCodeGroupHistoryRepository
 import kr.socar.code101.codebook.repository.ComCodeGroupRepository
 import kr.socar.code101.codebook.repository.ComCodeInfoRepository
 import kr.socar.code101.codebook.repository.ComCodeRepository
+import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.web.bind.annotation.GetMapping
@@ -19,7 +21,7 @@ class CodeAdminController(
     private val database: Database
 ) {
     //com_code_info CRU
-    @GetMapping("/com_code_info/new")
+    @GetMapping("/com_code_info/new") //C
     fun createNewComCodeInfo(
         @RequestParam("code_name") codeName: String,
         @RequestParam("description") description: String?
@@ -28,18 +30,18 @@ class CodeAdminController(
         return@transaction """{"code_id":${r.id.value}, "code_name":${r.codeName}, "description":${r.description}, "created":${r.createdAt} }"""
     }
 
-    @GetMapping("/com_code_info/list")
+    @GetMapping("/com_code_info/list") //R - all
     fun fetchComCodeInfo(): String = transaction(database) {
         return@transaction "[ " + ComCodeInfoRepository.findAll()
             .joinToString { "{ code_id:${it.id.value}, code_name:${it.codeName}, description:${it.description}, created:${it.createdAt}, updated:${it.updatedAt} } <br>".trimMargin() } + " ]"
     }
 
-    @GetMapping("/com_code_info/each")
+    @GetMapping("/com_code_info/each") //R
     fun fetchComCodeInfo(@RequestParam id: Int): String? = transaction(database) {
         return@transaction ComCodeInfoRepository.findOne(id)
     }
 
-    @GetMapping("/com_code_info/update")
+    @GetMapping("/com_code_info/update") //U
     fun modifyComCodeInfo(
         @RequestParam id: Int,
         @RequestParam description: String?
@@ -48,7 +50,7 @@ class CodeAdminController(
     }
 
     //com_code_group_history CR
-    @GetMapping("/com_code_group_history/new")
+    @GetMapping("/com_code_group_history/new")  //C
     fun createNewComCodeGroupHistory(
         @RequestParam("id") id: String,
         @RequestParam("name") codeGroupName: String
@@ -59,14 +61,45 @@ class CodeAdminController(
             "created_at:${r.createdAt}, updated_at:${r.updatedAt}"
     }
 
+    @GetMapping("/com_code_group_history/list") // R - all
+    fun fetchComCodeGroupHistory(): String = transaction(database) {
+        return@transaction "[ " + comCodeGroupHistoryRepository.findAll()
+                .joinToString {
+                    // 수정필요
+                    "code_group_id:${it.id.value}, code_validity_start_date:${it.validityStartDate}, code_validity_end_date:${it.validityEndDate}, <br>" +
+                    "bfchg_code_group_name:${it.bfchgCodeGroupName}, aftch_code_group_name:${it.aftchCodeGroupName}, <br>" +
+                    "created_at:${it.createdAt}, updated_at:${it.updatedAt} ] <br>"
+            }
+    }
+
+    @GetMapping("/com_code_group_history/each")     // R
+    fun fetchComCodeGroupHistory(@RequestParam id : String) : ComCodeGroupHistory? = transaction(database) {
+        return@transaction comCodeGroupHistoryRepository.findOne(id)
+    }
+
+
+    @GetMapping("/com_code_group_history/update")     // U
+    fun modifyComCodeGroupHistory(
+            @RequestParam id: String,
+            @RequestParam codeGroupName: String
+    ): String = transaction(database){
+        val result = comCodeGroupHistoryRepository.update(id, codeGroupName)
+        return@transaction if (result > 0) {
+            "success: $result"
+        } else {
+            "fail: $result"
+        }
+    }
+
+
     //com_code_group CRUD
-    @GetMapping("/comCodeGroupFind")
+    @GetMapping("/comCodeGroupFind")  //R - all
     fun comCodeGroupFind(): String = transaction(database) {
         val result = comCodeGroupRepository.findAll()
         return@transaction result.joinToString { comCodeGroup -> "id: ${comCodeGroup.id.value}, name: ${comCodeGroup.codeGroupName} </br>" }
     }
 
-    @GetMapping("/comCodeGroupInsert")
+    @GetMapping("/comCodeGroupInsert") //C
     fun comCodeGroup(
         @RequestParam("id") id: String,
         @RequestParam("name") codeGroupName: String
@@ -75,24 +108,17 @@ class CodeAdminController(
         return@transaction "id: ${result.id.value}, name: ${result.codeGroupName}, created: ${result.createdAt}"
     }
 
-    @GetMapping("/com_code_group_history/list")
-    fun fetchComCodeGroupHistory(): String = transaction(database) {
-        return@transaction "[ " + comCodeGroupHistoryRepository.findAll()
-            .joinToString {
-                "code_group_id:${it.id.value}, code_validity_start_date:${it.validityStartDate}, code_validity_end_date:${it.validityEndDate}, <br>" +
-                    "bfchg_code_group_name:${it.bfchgCodeGroupName}, aftch_code_group_name:${it.aftchCodeGroupName}, <br>" +
-                    "created_at:${it.createdAt}, updated_at:${it.updatedAt} ] <br>"
-            }
-    }
+    //U
+
 
     //com_code CRD
-    @GetMapping("/com_code/findAll")
+    @GetMapping("/com_code/findAll") //R - all
     fun fetchComCode(): String = transaction(database) {
         return@transaction "[ " + ComCodeRepository.findAll()
             .joinToString { "{ codeGroupId: ${it.codeGroupId}, code_id:${it.id.value}, useYN: ${it.useYN}, sortingNum: ${it.sortingNum}, created:${it.createdAt}, updated:${it.updatedAt} } <br>".trimMargin() } + " ]"
     }
 
-    @GetMapping("/com_code/new")
+    @GetMapping("/com_code/new") // C
     fun createNewComCode(
         @RequestParam codeGroupId: String,
         @RequestParam codeId: Int,
@@ -103,7 +129,7 @@ class CodeAdminController(
         return@transaction "codeGroupId: ${result.codeGroupId}, code_id:${result.id.value}, useYN: ${result.useYN}, sortingNum: ${result.sortingNum}, created:${result.createdAt}, updated:${result.updatedAt}"
     }
 
-    @GetMapping("/com_code/delete")
+    @GetMapping("/com_code/delete") // D
     fun deleteComCode(
         @RequestParam codeGroupId: String,
         @RequestParam codeId: Int
@@ -112,5 +138,4 @@ class CodeAdminController(
             .joinToString { "{ codeGroupId: ${it.codeGroupId}, code_id:${it.id.value}, useYN: ${it.useYN}, sortingNum: ${it.sortingNum}, created:${it.createdAt}, updated:${it.updatedAt} } <br>".trimMargin() } + " ]"
     }
 
-    //com_code_group_history CR
 }
--- src/main/kotlin/kr/socar/code101/codebook/repository/ComCodeGroupHistoryRepository.kt
+++ src/main/kotlin/kr/socar/code101/codebook/repository/ComCodeGroupHistoryRepository.kt
@@ -2,7 +2,11 @@ package kr.socar.code101.codebook.repository
 
 import kr.socar.code101.codebook.model.ComCodeGroupHistory
 import kr.socar.code101.codebook.model.ComCodeGroupHistorys
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.update
 import org.springframework.stereotype.Repository
 import java.time.Clock
 import java.time.LocalDateTime
@@ -10,20 +14,34 @@ import java.time.LocalDateTime
 
 @Repository
 class ComCodeGroupHistoryRepository(private val clock: Clock) {
-    val now = LocalDateTime.now(clock)
-
     fun findAll(): List<ComCodeGroupHistory> {
         val query = ComCodeGroupHistorys.selectAll()
         return ComCodeGroupHistory.wrapRows(query).toList()
     }
 
     fun insert(id: String, codeGroupName: String): ComCodeGroupHistory {
+        val now = LocalDateTime.now(clock)
         return ComCodeGroupHistory.new(id) {
             this.validityStartDate = now
             this.validityEndDate = LocalDateTime.of(9999, 12, 31, 11, 59)
             this.bfchgCodeGroupName = codeGroupName
             this.createdAt = now
             this.updatedAt = now
+        }
+    }
+
+    fun findOne(id: String): ComCodeGroupHistory? {
+        val query = ComCodeGroupHistorys.select { ComCodeGroupHistorys.id eq id }
+        return ComCodeGroupHistory.wrapRows(query).firstOrNull()
+    }
+
+    fun update(id: String, codeGroupName : String) : Int {
+        val now = LocalDateTime.now(clock)
+        return ComCodeGroupHistorys.update({
+            ComCodeGroupHistorys.id eq id
+        }) {
+            it[this.bfchgCodeGroupName] = codeGroupName
+            it[this.updatedAt] = now
         }
     }
 }
