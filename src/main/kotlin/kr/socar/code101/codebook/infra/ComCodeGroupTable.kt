6f7f7bae759f1fa56844549dd16891fab70dc116
socar-matt <matt@socar.kr>
2021-07-31 14:46:21 +0900

    fix: ComCodeGroupTable 수정
    - code_group_name의 유니크 인덱스 삭제

--- src/main/kotlin/kr/socar/code101/codebook/infra/ComCodeGroupTable.kt
+++ src/main/kotlin/kr/socar/code101/codebook/infra/ComCodeGroupTable.kt
@@ -7,7 +7,7 @@ import java.time.LocalDateTime
 
 object ComCodeGroupTable : Table("com_code_group") {
     val codeGroupId: Column<String> = varchar("code_group_id", 4)
-    val codeGroupName: Column<String> = varchar("code_group_name", 100).uniqueIndex()
+    val codeGroupName: Column<String> = varchar("code_group_name", 100)
     val parentCodeGroupId: Column<String?> =
         (varchar("parent_code_group_id", 4) references codeGroupId).nullable().default(null)
     val codeGroupDescription: Column<String?> = varchar("code_group_description", 200).nullable().default(null)
