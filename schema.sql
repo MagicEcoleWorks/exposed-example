506f4b347c739db3d1b12a433ca475a16abaf51b
socar-matt <matt@socar.kr>
2021-07-31 14:38:19 +0900

    fix: 스프레드시트에 맞춰서 쿼리 수정
    - 궁금하거나 확실히 모르는 부분은 TODO 처리

--- schema.sql
+++ schema.sql
@@ -1,25 +1,24 @@
 CREATE TABLE `com_code_info` (
-                                 `code_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '코드ID|코드ID|',
-                                 `code_name` varchar(50) NOT NULL COMMENT '코드명|코드명|',
-                                 `description` varchar(200) DEFAULT NULL COMMENT '설명|코드에 대한 설명|',
+                                 `code_id` varchar(3) NOT NULL AUTO_INCREMENT COMMENT '코드ID|코드ID|',
+                                 `code_name` varchar(64) NOT NULL COMMENT '코드명|코드명|',
                                  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일시|생성일시|',
                                  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '생성일시|생성일시|',
                                  PRIMARY KEY (`code_id`),
-                                 UNIQUE KEY `com_code_info_ux01` (`code_name`)
+                                    -- TODO 이건...어떻게...? 여기도 원래 UNIQUE 가 있었다.
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='공통코드정보|공통코드정보|';
 
 
 CREATE TABLE `com_code_group` (
                                   `code_group_id` varchar(4) NOT NULL COMMENT '코드그룹ID|코드그룹ID|',
-                                  `upper_code_group_id` varchar(4) DEFAULT NULL COMMENT '상위코드그룹ID|상위코드그룹ID|',
-                                  `code_group_name` varchar(50) NOT NULL COMMENT '코드그룹명|코드그룹명|',
-                                  `code_group_description` varchar(200) DEFAULT NULL COMMENT '코드그룹설명|코드그룹설명|',
+                                  `code_group_name` varchar(100) NOT NULL COMMENT '코드그룹명|코드그룹명|',
+                                  `parent_code_group_id` varchar(4) DEFAULT NULL COMMENT '상위코드그룹ID|상위코드그룹ID|',
+                                  `description` varchar(200) DEFAULT NULL COMMENT '설명|설명|',
                                   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일시|생성일시|',
                                   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시|수정일시|',
                                   PRIMARY KEY (`code_group_id`),
-                                  UNIQUE KEY `com_code_group_ux01` (`code_group_name`),
-                                  KEY `com_code_group_fk01` (`upper_code_group_id`),
-                                  CONSTRAINT `com_code_group_fk01` FOREIGN KEY (`upper_code_group_id`) REFERENCES `com_code_group` (`code_group_id`)
+                                  UNIQUE KEY `com_code_group_ux01` (`code_group_name`), -- TODO 이건...어떻게...?
+                                  KEY `com_code_group_fk01` (`parent_code_group_id`),
+                                  CONSTRAINT `com_code_group_fk01` FOREIGN KEY (`parent_code_group_id`) REFERENCES `com_code_group` (`code_group_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='공통코드그룹|공통 코드에 대한 그룹|';
 
 
@@ -27,19 +26,19 @@ CREATE TABLE `com_code_group_history` (
                                           `code_group_id` varchar(4) NOT NULL COMMENT '코드그룹ID|com_code_group.id|',
                                           `code_validity_start_date` date NOT NULL COMMENT '코드유효시작일자|코드유효시작일자|',
                                           `code_validity_end_date` date NOT NULL COMMENT '코드유효종료일자|코드유효종료일자|',
-                                          `bfchg_code_group_name` varchar(50) NOT NULL COMMENT '변경전코드그룹명|변경전 코드그룹명|',
-                                          `aftch_code_group_name` varchar(50) DEFAULT NULL COMMENT '변경후코드그룹명|변경후그룹코드명|',
+                                          `code_group_name` varchar(50) NOT NULL COMMENT '코드그룹명|코드그룹명|',
                                           `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일시|생성일시|',
                                           `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시|수정일시|',
-                                          PRIMARY KEY (`code_group_id`,`code_validity_start_date`),
+                                          PRIMARY KEY (`code_group_id`,`code_validity_start_date`), -- TODO 이건...어떻게...?
                                           CONSTRAINT `com_code_group_history_fk01` FOREIGN KEY (`code_group_id`) REFERENCES `com_code_group` (`code_group_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='공통dj코드그룹이력';
 
 
 CREATE TABLE `com_code` (
                             `code_group_id` varchar(4) NOT NULL COMMENT '코드그룹ID|com_code_group.id|',
-                            `code_id` int(11) NOT NULL COMMENT '코드ID|com_code_info.id|',
+                            `code_id` varchar(3) NOT NULL COMMENT '코드ID|com_code_info.id|',
                             `use_yn` tinyint(1) NOT NULL COMMENT '사용여부|사용여부|',
+                            `description` varchar(200) DEFAULT NULL COMMENT '설명|설명|',
                             `sorting_number` int(5) NOT NULL COMMENT '정렬번호|코드그룹별 정렬번호를 생성|',
                             `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일시|생성일시|',
                             `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '생성일시|생성일시|',
